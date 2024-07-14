579b01df24a11ffa9bd85b213cafdf3adf6e812a
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2024-07-14 22:55:47 +0200

    add pgboss

---
+++ app/pgboss/api/route.ts
@@ -0,0 +1,31 @@
+import PgBoss from "pg-boss";
+export const POST = async (req: Request) => {
+  const start = performance.now();
+  const { task } = await req.json();
+  const boss = new PgBoss(process.env.PGBOSS_URL!);
+
+  boss.on("error", (error) => console.error(error));
+
+  await boss.start();
+
+  const queue = "job-1";
+
+  let jobId = await boss.send(queue, { task });
+
+  console.log(`created job in queue ${queue}: ${jobId}`);
+
+  await boss.work(queue, someAsyncJobHandler);
+
+  const end = performance.now();
+  console.log(`handler took ${end - start}ms`);
+
+  return Response.json({ jobId });
+};
+
+async function someAsyncJobHandler(job: PgBoss.Job<{ task: string }>) {
+  console.log(`staring job ${job.id}`);
+
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
+  console.log(`job ${job.id} completed: ${job.data.task}`);
+}
