34f8cac6db9fbbeebd011bfbe80fc88e0544df23
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2024-07-14 23:38:16 +0200

    add migration

---
+++ prisma/migrations/20240714213751_add_log_table/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "logs" (
+    "id" SERIAL NOT NULL,
+    "data" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "logs_pkey" PRIMARY KEY ("id")
+);
