8a2c0619ad0165bf91b61be9a4774cc7e60aa541
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2024-09-27 14:23:33 +0200

    Refactor to use `use-push-router` package, removing the old `usePushRoute` hook and updating imports accordingly

--- app/search-params/page.tsx
+++ app/search-params/page.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { urlSearchParamsToObject, usePushRoute } from "@/lib/use-push-route";
+import { urlSearchParamsToObject, usePushRoute } from "use-push-router";
 import { XIcon } from "lucide-react";
 import { useState } from "react";
 
